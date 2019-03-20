985dcad35692160a5f84208b41d1889f30d14be0
Jonny Rodriguez <jojoroto@gmail.com>
2019-03-20 01:21:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,7 @@
 var theBeatlesPlay = (musicians,instruments) => {
   let strings = [];
   for( let i=0; i<instruments.length; i++ ) {
-    for( let m=0; m<musicians.length; m++ ) {
-      strings.push(musicians[m]+" plays "+instruments[i]);
-    }
+      strings.push(musicians[i]+" plays "+instruments[i]);
   }
   return strings;
 };
